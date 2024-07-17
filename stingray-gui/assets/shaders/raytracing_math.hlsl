eaaa73d4decce0f7072ba7fc9ac391de9780d048
JackiBackiBoy <thegametail@gmail.com>
2024-07-18 00:21:17 +0200

    Improved random sampling efficiency with cosine weighted distribution

--- stingray-gui/assets/shaders/raytracing_math.hlsl
+++ stingray-gui/assets/shaders/raytracing_math.hlsl
@@ -19,6 +19,47 @@ Vertex barycentricLerp(in Vertex v0, in Vertex v1, in Vertex v2, in float3 baryc
 }
 
 /* Random Functions */
+uint initRand(in uint v0, in uint v1, in uint backoff = 16) {
+    uint s0 = 0;
+
+    for (uint n = 0; n < backoff; n++) {
+		s0 += 0x9e3779b9;
+		v0 += ((v1 << 4) + 0xa341316c) ^ (v1 + s0) ^ ((v1 >> 5) + 0xc8013ea4);
+		v1 += ((v0 << 4) + 0xad90777d) ^ (v0 + s0) ^ ((v0 >> 5) + 0x7e95761e);
+	}
+	return v0;
+}
+
+float nextRand(inout uint seed) {
+	seed = (1664525u * seed + 1013904223u);
+	return float(seed & 0x00FFFFFF) / float(0x01000000);
+}
+
+// Utility function to get a vector perpendicular to an input vector 
+//    (from "Efficient Construction of Perpendicular Vectors Without Branching")
+float3 getPerpendicularVector(float3 u)
+{
+	float3 a = abs(u);
+	uint xm = ((a.x - a.y)<0 && (a.x - a.z)<0) ? 1 : 0;
+	uint ym = (a.y - a.z)<0 ? (1 ^ xm) : 0;
+	uint zm = 1 ^ (xm | ym);
+	return cross(u, float3(xm, ym, zm));
+}
+
+float3 getRandCosHemisphereSample(in float3 normal, inout uint seed) {
+    // Get 2 random numbers to select our sample with
+	float2 randVal = float2(nextRand(seed), nextRand(seed));
+
+	// Cosine weighted hemisphere sample from RNG
+	float3 bitangent = getPerpendicularVector(normal);
+	float3 tangent = cross(bitangent, normal);
+	float r = sqrt(randVal.x);
+	float phi = 2.0f * 3.14159265f * randVal.y;
+
+	// Get our cosine-weighted hemisphere lobe sample direction
+	return tangent * (r * cos(phi).x) + bitangent * (r * sin(phi)) + normal.xyz * sqrt(1 - randVal.x);
+}
+
 uint xorShift32(inout uint state) {
     state ^= (state << 13);
     state ^= (state >> 17);
